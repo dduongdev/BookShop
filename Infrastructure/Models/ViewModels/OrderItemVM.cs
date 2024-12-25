fab9fd359962c4867e5943bdaa9f5c36e0fa267d
Peekapoo-217 <conghung2172004@gmail.com>
2024-12-25 17:05:14 +0700

    Create UserController, OrderMapping

---
+++ Infrastructure/Models/ViewModels/OrderItemVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Models.ViewModels
+{
+    public class OrderItemVM
+    {
+        public int Id { get; set; }
+        public string BookTitle { get; set; }
+        public required int Quantity { get; set; }
+    }
+}
---
+++ Infrastructure/Services/OrderMappingService.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+using Infrastructure.Models.ViewModels;
+using UseCases;
+
+namespace Infrastructure.Services
+{
+    public class OrderMappingService
+    {
+        private readonly BookManager _bookManager;
+        public async Task<OrderVM> MaptoOrderVM(Order order)
+        {
+            var orderVM = new OrderVM
+            {
+                Id = order.Id,
+                DeliveryAddress = order.DeliveryAddress,
+                DeliveryPhone = order.DeliveryPhone,
+                CreatedAt = order.CreatedAt,
+                PaymentMethod = order.PaymentMethod,
+                PaymentStatus = order.PaymentStatus,
+                orderItemVMs = new List<OrderItemVM>()
+            };
+
+            foreach (var item in order.OrderItems)
+            {
+                var book = await _bookManager.GetByIdAsync(item.BookId);
+                string bookTitle = book?.Title ?? "Unknow Title";
+
+                orderVM.orderItemVMs.Add(new OrderItemVM
+                {
+                    Id = item.Id,
+                    BookTitle = bookTitle,
+                    Quantity = item.Quantity
+                });
+            }
+            return orderVM;
+        }
+    }
+}
